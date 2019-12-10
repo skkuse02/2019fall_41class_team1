a5a5701d5b5662ec341a098ded1b0d353cbb5e73
k12340k <k12340k@naver.com>
2019-12-10 20:32:40 +0900

    FoodRecommendation, FoodUpload 커밋
    각각의 자바파일, xml파일과 app 단위, project 단위 gradle 수정

--- app/src/main/java/edu/skku/SE/balancedone/FoodRecommendation.java
+++ app/src/main/java/edu/skku/SE/balancedone/FoodRecommendation.java
@@ -1,13 +1,83 @@
 package edu.skku.SE.balancedone;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.github.mikephil.charting.charts.RadarChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.RadarData;
+import com.github.mikephil.charting.data.RadarDataSet;
+import com.github.mikephil.charting.data.RadarEntry;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
 
 public class FoodRecommendation extends AppCompatActivity {
+
+    RadarChart RadarChart;
+    RadarData radarData;
+    RadarDataSet radarDataSet;
+    ArrayList radarEntries;
+    TextView RecommendedFood;
+    ImageView pastFood1;
+    ImageView pastFood2;
+    ImageView pastFood3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_foodrecommendation);
+
+        pastFood1 = findViewById(R.id.imageView);
+        pastFood2 = findViewById(R.id.imageView2);
+        pastFood3 = findViewById(R.id.imageView3);
+
+        /*                                                                                          //주석 풀고 이전에 먹은 음식 사진 입력(현재 기본이미지)
+        pastFood1.setImageBitmap();
+        pastFood2.setImageBitmap();
+        pastFood3.setImageBitmap();
+        */
+
+        RadarChart = findViewById(R.id.RadarChart);
+        RecommendedFood = findViewById(R.id.textView);
+        String Foodname = "Pizza";                                                                 //추천음식명 입력(현재 그냥 pizza로 설정)
+        String RecommendString = "We Recommend " + Foodname;
+        RecommendedFood.setText(RecommendString);
+        RecommendedFood.setTextSize(30f);
+
+        getEntries();
+        radarDataSet = new RadarDataSet(radarEntries, "Nutrition Status");
+        RadarChart.getDescription().setEnabled(false);
+        RadarChart.setRotationAngle(60);
+        RadarChart.setRotationEnabled(false);
+        String[] labels = {"Calcium","VitaminA","VitaminB","VitaminC","Protein","Calorie"};
+        XAxis xAxis = RadarChart.getXAxis();
+        xAxis.setValueFormatter(new IndexAxisValueFormatter(labels));
+        xAxis.setTextSize(15);
+        YAxis yAxis = RadarChart.getYAxis();
+        yAxis.setAxisMinimum(0);
+        radarData = new RadarData(radarDataSet);
+        RadarChart.setData(radarData);
+        RadarChart.getLegend().setEnabled(false);
+        radarDataSet.setColors(Color.BLUE);                                                       //그래프 색, 가독성 측면에서 파랑으로 설정해둠
+        radarDataSet.setValueTextColor(Color.BLACK);
+        radarDataSet.setValueTextSize(15);
     }
+
+    private void getEntries() {
+        radarEntries = new ArrayList<>();
+        radarEntries.add(new RadarEntry(3, "cal"));                                  //칼슘량 value에 입력 (현재 임의의 값 들어있음)
+        radarEntries.add(new RadarEntry(3, "vitA"));                                 //비타민A양 value에 입력 (현재 임의의 값 들어있음)
+        radarEntries.add(new RadarEntry(4, "vitB"));                                 //비타민B양 value에 입력 (현재 임의의 값 들어있음)
+        radarEntries.add(new RadarEntry(5, "vitC"));                                 //비타민C양 value에 입력 (현재 임의의 값 들어있음)
+        radarEntries.add(new RadarEntry(3, "pro"));                                  //단백질량 value에 입력 (현재 임의의 값 들어있음)
+        radarEntries.add(new RadarEntry(4, "calo"));                                 //칼로리양 value에 입력 (현재 임의의 값 들어있음)
+    }
+
 }
--- app/src/main/java/edu/skku/SE/balancedone/FoodUpload.java
+++ app/src/main/java/edu/skku/SE/balancedone/FoodUpload.java
@@ -1,13 +1,113 @@
 package edu.skku.SE.balancedone;
 
+import android.Manifest;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Matrix;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.gun0912.tedpermission.PermissionListener;
+import com.gun0912.tedpermission.TedPermission;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class FoodUpload extends AppCompatActivity {
+
+    ImageView image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_foodupload);
+
+        image = findViewById(R.id.imageView);
+
+        tedPermission();
+
+        findViewById(R.id.btnGallery).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                imageFromGallery();
+            }
+        });
+
+        findViewById(R.id.btnCamera).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                imageFromCamera();
+            }
+        });
+    }
+
+    private void imageFromGallery() {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
+        startActivityForResult(intent, 1);
+    }
+
+    private void imageFromCamera(){
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, 2);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent){
+        Uri filepath;
+        if(requestCode == 1){
+            filepath = intent.getData();
+            if(filepath != null){
+                try{
+                    Bitmap photo = MediaStore.Images.Media.getBitmap(getContentResolver(), filepath);
+                    image.setImageBitmap(photo);
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
         }
+        else if(requestCode == 2){
+            Bitmap photo = (Bitmap) intent.getExtras().get("data");
+            if(photo != null){
+                int scale = 2000;
+
+                float width = photo.getWidth();
+                float height = photo.getHeight();
+
+                float newHightf = (scale*height)/width;
+                int newHeight = (int) newHightf;
+                Bitmap photoResize = Bitmap.createScaledBitmap(photo, scale, newHeight, true);
+                photo.recycle();
+
+                Matrix matrix = new Matrix();
+                matrix.postRotate(360);
+                Bitmap photoRotate = Bitmap.createBitmap(photoResize, 0, 0, photoResize.getWidth(), photoResize.getHeight(), matrix, false);
+                photoResize.recycle();
+
+                image.setImageBitmap(photoRotate);
+            }
+        }
+    }
+
+    private void tedPermission() {
+
+        PermissionListener permissionListener = new PermissionListener() {
+            @Override
+            public void onPermissionGranted() {
+            }
+
+            @Override
+            public void onPermissionDenied(ArrayList<String> deniedPermissions) {
+            }
+        };
+
+        TedPermission.with(this)
+                .setPermissionListener(permissionListener)
+                .setDeniedMessage("Denied")
+                .setPermissions(Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.CAMERA)
+                .check();
+    }
 }
